45eacc7f89488f2112178f91aa9d41c0c8858b28
Pedro Lopes <83576881+pedrompflopes@users.noreply.github.com>
2024-09-27 10:51:40 +0100

    Update my-backend.php

--- service-php/my-backend.php
+++ service-php/my-backend.php
@@ -8,7 +8,7 @@
 /// XSS example
 //
 $var = $_POST['var'];
-echo "<div>$var</div>\n";
+//echo "<div>$var</div>\n";
 
 
 //
@@ -21,7 +21,7 @@ echo "<div>$varB</div>\n";
 /// Forget to terminate user input after a redirect
 //
 if ($_SESSION['user_logged_in'] !== true) {
-  header('Location: /login.php');
+  //header('Location: /login.php');
 }
 
 // Important private logic that shouldn't happen because we've already redirected the user!
